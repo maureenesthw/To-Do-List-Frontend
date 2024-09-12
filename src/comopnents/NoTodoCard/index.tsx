2fb82a89cfc4d6aba047ca04fa6c6b62ec38d769
maureenesthw <maureen.esther.wijaya@gmail.com>
2024-09-13 02:48:08 +0700

    feat: NoTodoCard component

--- src/comopnents/NoTodoCard/index.tsx
+++ src/comopnents/NoTodoCard/index.tsx
@@ -1,6 +1,6 @@
 const NoTodoCard = () => {
   return (
-    <div className="bg-white shadow shadow-sm text-sm leading-5 font-medium text-stone-700 h-28 flex justify-center items-center">
+    <div className="bg-white rounded-lg shadow shadow-sm text-sm leading-5 font-medium text-stone-700 h-28 flex justify-center items-center">
       <p>Nothing to-do yet.</p>
     </div>
   );
